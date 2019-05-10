53677378fa0ad9ebdeef7ed9a0ad1f22a28e33f4
Nicolas Chen <35080480+nicolaschen1@users.noreply.github.com>
2019-05-11 00:43:07 +0200

    Add files via upload

---
+++ Store/Reducers/avatarReducer.js
@@ -0,0 +1,20 @@
+//Developer: Nicolas Chen
+// Store/Reducers/avatarReducer.js
+
+const initialState = { avatar: require('../../Images/ic_tag_faces.png') }
+
+function setAvatar(state = initialState, action) {
+  let nextState
+  switch (action.type) {
+    case 'SET_AVATAR':
+      nextState = {
+        ...state,
+        avatar: action.value
+      }
+      return nextState || state
+  default:
+    return state
+  }
+}
+
+export default setAvatar
